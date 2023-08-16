0baa72aef9c864157b99d92dc0e09bc0262fab1f
Vinicius Quinafelex Alves <vqalves@hotmail.com>
2023-08-16 00:28:44 -0300

    Added domain to sitemap.xml

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -2,10 +2,12 @@ import ArticleSource from "../sources/article.source";
 
 const Sitemap = () => {};
 
+const domain = "https://www.vinafelex.com.br";
+
 const createSitemap = (urls) => 
   `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map((url) => `<url><loc>${url}</loc></url>`).join("")}
+${urls.map((url) => `<url><loc>${domain}${url}</loc><changefreq>daily</changefreq></url>`).join("")}
 </urlset>`;
 	
 export async function getServerSideProps({ res }) {      
