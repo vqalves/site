b24d3b5a56d8f630248071fc932ff7f4cf26eb58
Vinicius Quinafelex Alves <vqalves@hotmail.com>
2023-08-15 23:59:30 -0300

    Added sitemap.xml

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,26 @@
+import ArticleSource from "../sources/article.source";
+
+const Sitemap = () => {};
+
+const createSitemap = (urls: string[]) => 
+  `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map((url) => `<url><loc>${url}</loc></url>`).join("")}
+</urlset>`;
+	
+export async function getServerSideProps({ res, req }) {      
+	// const siteMapJson = await fetch(`https://www.exampleapi.com/getsitemap`);
+	// const urlList = await siteMapJson.json();
+
+    var urls = [ "", "/articles", "/projects" ];
+    urls = urls.concat(ArticleSource.listAll().map(x => x.getRoute()));
+    urls = urls.concat(urls.map(x => `/en-US${x}`));
+
+	const sitemap = createSitemap(urls);
+    
+	res.setHeader("Content-Type", "text/xml");
+	res.write(sitemap);
+	res.end();
+	return { props: { results : {}}}
+};
+export default Sitemap;
