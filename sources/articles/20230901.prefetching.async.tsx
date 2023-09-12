8cbf33e8bb11f1f83205e73312d076091cd7a14c
Vinicius Quinafelex Alves <vqalves@hotmail.com>
2023-09-12 11:39:26 -0300

    Added titles and PT-BR translation

--- sources/articles/20230901.prefetching.async.tsx
+++ sources/articles/20230901.prefetching.async.tsx
@@ -26,12 +26,12 @@ export const PrefetchingAsync20230901 = new Article({
     
     title: new LocaleContentText({
         en: "[C#] Prefetching async methods for performance",
-        pt: ""
+        pt: "[C#] Prefetching de métodos assíncronos"
     }),
 
     description: new LocaleContentText({
         en: "Starting I/O operations before needed can help speed up the runtime",
-        pt: ""
+        pt: "Iniciar operações com I/O o quanto antes pode agilizar o tempo de execução"
     }),
 
     slug: new LocaleContentText({
@@ -44,17 +44,17 @@ export const PrefetchingAsync20230901 = new Article({
     getContent: () => [
         new LocaleContentAny({
             en: (<p><ExternalLink href="https://en.wikipedia.org/wiki/Prefetching">Prefetching</ExternalLink> is a technique that starts loading data before it is needed, reducing total runtime at the risk of loading unnecessary data.</p>),
-            pt: (<p></p>)
+            pt: (<p><ExternalLink href="https://pt.wikipedia.org/wiki/Busca_antecipada_de_instru%C3%A7%C3%B5es">Prefetching</ExternalLink> (ou pré-carregamento) é uma técnica de iniciar o carregamento de dados antes deles serem necessários, diminuindo o tempo total de execução, mas com o risco de carregar dados desnecessariamente.</p>)
         }),
 
         new LocaleContentAny({
             en: (<p>In coding, since there is more knowledge over what is needed to execute a method, it is easier to control when certain data should be loaded or not.</p>),
-            pt: (<p></p>)
+            pt: (<p>Na programação, através do código, o desenvolvedor pode identificar quais dados serão necessários em quais situações, reduzindo ou eliminando o risco de carregamentos desnecessários.</p>)
         }),
 
         new LocaleContentAny({
-            en: (<p>On C#, using <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/async-scenarios">asynchrony</ExternalLink> and <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task?view=net-7.0">Task</ExternalLink> allows fetching data without interrupting the flow of code. This way, by prefetching data, the algorithm can dilute the downtime of I/O operations by working on other operations while waiting for the I/O result.</p>),
-            pt: (<p></p>)
+            en: (<p>On C#, using <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/async-scenarios">async</ExternalLink> and <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task?view=net-7.0">tasks</ExternalLink> allow fetching data without interrupting the flow of code. This way, by prefetching data, the algorithm can dilute the downtime of I/O operations by working on other operations while waiting for the I/O result.</p>),
+            pt: (<p>No C#, usar <ExternalLink href="https://learn.microsoft.com/pt-br/dotnet/csharp/asynchronous-programming/async-scenarios">async</ExternalLink> e <ExternalLink href="https://learn.microsoft.com/pt-br/dotnet/api/system.threading.tasks.task?view=net-7.0">tasks</ExternalLink> permite iniciar o carregamento de dados sem interromper o fluxo do código. Isso ajuda a diluir o tempo que o algoritmo ficará esperando pelo resultado de operações de I/O, pois ele estará trabalhando em outros comandos enquanto espera o resultado.</p>)
         }),
 
         LocaleContentAny.all(<CodeBlock
@@ -76,8 +76,13 @@ var html = await taskHtml
 CodeWithHtml(html);`}></CodeBlock>),
 
         new LocaleContentAny({
+            en: (<h3>Prefetching multiple results</h3>),
+            pt: (<h3>Executando múltiplos pré-carregamentos</h3>)
+        }),
+
+        new LocaleContentAny({
             en: (<p>Multiple prefetches can be started in sequence.</p>),
-            pt: (<p></p>)
+            pt: (<p>Múltiplos pré-carregamentos podem ser iniciados em sequência.</p>)
         }),
 
         LocaleContentAny.all(<CodeBlock
@@ -93,8 +98,13 @@ var html2 = await taskHtml2;
 var html3 = await taskHtml3;`}></CodeBlock>),
 
         new LocaleContentAny({
-            en: (<p>Tasks can also be chained through the use of <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.continuewith?view=net-7.0">ContinueWith()</ExternalLink>, which invokes a method that starts executing as soon as a task is completed, and <ExternalLink href="https://learn.microsoft.com/pt-br/dotnet/api/system.threading.tasks.taskextensions.unwrap?view=net-7.0">Unwrap()</ExternalLink>, which will expose the chained task being executed inside ContinueWith. Note that, since ContinueWith will only start when the task is completed, calling <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task-1.result?view=net-7.0">.Result</ExternalLink> property will not block the thread.</p>),
-            pt: (<p></p>)
+            en: (<h3>Chained prefetching</h3>),
+            pt: (<h3>Encadeamento de prefetching</h3>)
+        }),
+
+        new LocaleContentAny({
+            en: (<p>Tasks can also be chained through the use of <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.continuewith?view=net-7.0">ContinueWith()</ExternalLink>, which invokes a method that starts executing as soon as a task is completed, and <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.taskextensions.unwrap?view=net-7.0">Unwrap()</ExternalLink>, which will expose the chained task being executed inside ContinueWith. Note that, since ContinueWith will only start when the task is completed, calling <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task-1.result?view=net-7.0">.Result</ExternalLink> property will not block the thread.</p>),
+            pt: (<p>Tasks também podem ser encadeadas usando <ExternalLink href="https://learn.microsoft.com/pt-br/dotnet/api/system.threading.tasks.task.continuewith?view=net-7.0">ContinueWith()</ExternalLink>, que invocará um método qualquer assim que a task for concluída, e <ExternalLink href="https://learn.microsoft.com/pt-br/dotnet/api/system.threading.tasks.taskextensions.unwrap?view=net-7.0">Unwrap()</ExternalLink>, que expõe a task interna sendo executada no ContinueWith. Considerando que o ContinueWith só é executado depois que a task foi concluída, não há bloqueio de thread quando é chamado diretamente o <ExternalLink href="https://learn.microsoft.com/pt-br/dotnet/api/system.threading.tasks.task-1.result?view=net-7.0">.Result</ExternalLink> da task.</p>)
         }),
 
         LocaleContentAny.all(<CodeBlock
@@ -117,8 +127,13 @@ var statusCode = await taskStatusCode;
 var hasFavIcon = await taskFavicon;`}></CodeBlock>),
 
         new LocaleContentAny({
-            en: (<p>When using <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.iasyncenumerable-1?view=net-7.0">IAsyncEnumerable</ExternalLink>, it is also possible to prefetch the next result by manipulating the <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/api/system.collections.ienumerator?view=net-7.0">IEnumerable</ExternalLink>, as demonstrated by the following <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods">extension method</ExternalLink>:</p>),
-            pt: (<p></p>)
+            en: (<h3>IAsyncEnumerable</h3>),
+            pt: (<h3>IAsyncEnumerable</h3>)
+        }),
+
+        new LocaleContentAny({
+            en: (<p>When using <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.iasyncenumerable-1?view=net-7.0">IAsyncEnumerable</ExternalLink>, it is also possible to prefetch the next result by manipulating the <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/api/system.collections.ienumerator?view=net-7.0">IEnumerator</ExternalLink>, as demonstrated by the following <ExternalLink href="https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods">extension method</ExternalLink>:</p>),
+            pt: (<p>Quando utilizar <ExternalLink href="https://learn.microsoft.com/pt-br/dotnet/api/system.collections.generic.iasyncenumerable-1?view=net-7.0">IAsyncEnumerable</ExternalLink>, também é possível pré-carregar o próximo resultado ao controlar o <ExternalLink href="https://learn.microsoft.com/pt-br/dotnet/api/system.collections.ienumerator?view=net-7.0">IEnumerator</ExternalLink>, como demonstrado pelo <ExternalLink href="https://learn.microsoft.com/pt-br/dotnet/csharp/programming-guide/classes-and-structs/extension-methods">extension method</ExternalLink> abaixo:</p>)
         }),
 
         LocaleContentAny.all(<CodeBlock
